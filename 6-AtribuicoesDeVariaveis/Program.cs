b54da2d43562f8b2c0dca1d207ab512c3ac2ccf4
Gutemberg Nascimento da Silva <iblue.gnsilva@terceiros.mag.com.br>
2021-08-16 20:43:46 -0300

    Projeto 6

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20; //vai sobscrever o primeiro valor
+
+            Console.WriteLine(idade);// idade será igual a 20
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+}
