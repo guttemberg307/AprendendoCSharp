8f183ec8f83179a70919fae4d416fa9d33569ec4
Gutemberg Nascimento da Silva <iblue.gnsilva@terceiros.mag.com.br>
2021-08-16 08:39:27 -0300

    CaracterETextos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';// aspas simples para representar somente um caracter
+            Console.WriteLine(primeiraLetra);
+
+
+            // tem como parametro ascii table 65 representa a letra A 
+            primeiraLetra = (char)65; // (char) entre parenteses é chamado de castig que convertera int em char 
+            Console.WriteLine(primeiraLetra);
+
+            // 66 representa a letra B
+            primeiraLetra =(char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia " + 2021;
+            string cursosProgamacao =//tudo a frente do @ vira caracter 
+ @" - .NET
+ - Java
+ - Javascript"; 
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgamacao);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
